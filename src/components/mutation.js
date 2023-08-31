7f92663ebd591a391773504b78c9eaa47fc9b4a0
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-08-31 19:34:59 +0600

    mutation added

---
+++ src/components/mutation.js
@@ -0,0 +1,16 @@
+let demonSlayer = [
+    'Tanjiro',
+    'Rengoku',
+    'Tengen',
+    'Openai',
+    'Mitsuri',
+    'Shinobu'
+]
+
+// demonSlayer.push('Zenetsu');
+
+// demonSlayer = ['Zenetsu', 'Inoske'];
+
+// const hashiras = ['Zenetsu', 'Inoske' ...demonSlayer];
+
+console.log(demonSlayer);
